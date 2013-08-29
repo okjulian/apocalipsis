9139019206a5778fbb551476b48c9e7c6f1d6a1b
JulianMayorga <mayorga.julian@gmail.com>
2013-08-29 17:06:45 -0300

    Esqueleto armado con phaser

---
+++ js/apocalipsis.js
@@ -0,0 +1,17 @@
+(function () {
+    // Create game instance and connect init, create and update methods
+    var myGame = new Phaser.Game(this, 'game', 480, 800, init, create, update);
+
+    function init() {
+
+    }
+
+    function create() {
+
+    }
+
+    function update() {
+
+    }
+
+}());
