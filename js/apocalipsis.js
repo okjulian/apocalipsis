a084500c657153da2eccbb3c4c1e89d47283bf99
JulianMayorga <mayorga.julian@gmail.com>
2013-08-29 18:43:33 -0300

    Agregar mockup de tierra y test de touch

--- js/apocalipsis.js
+++ js/apocalipsis.js
@@ -1,17 +1,19 @@
-(function () {
     // Create game instance and connect init, create and update methods
     var myGame = new Phaser.Game(this, 'game', 480, 800, init, create, update);
 
     function init() {
-
+        myGame.loader.addImageFile('earth', 'img/earth.png');
+        myGame.loader.load();
     }
 
-    function create() {
+    var earth;
 
+    function create() {
+        earth = myGame.createSprite(40, 200, 'earth');
     }
 
     function update() {
-
+        if (myGame.input.touch.isDown) {
+            alert('Touch!');
+        }
     }
-
-}());
